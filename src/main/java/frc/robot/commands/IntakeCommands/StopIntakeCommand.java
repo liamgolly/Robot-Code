5fdf29b8b061ab5f672630c7e46de3f27fa12c1e
liam <liam.golly@gmail.com>
2020-02-20 21:10:26 -0800

    added curvature drive, fixed standard driving operations

---
+++ src/main/java/frc/robot/commands/IntakeCommands/StopIntakeCommand.java
@@ -0,0 +1,35 @@
+package frc.robot.commands.IntakeCommands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.IntakeSubsystem;
+
+
+public class StopIntakeCommand extends CommandBase {
+    private final IntakeSubsystem intakeSubsystem;
+
+    public StopIntakeCommand(IntakeSubsystem intakeSubsystem) {
+        this.intakeSubsystem = intakeSubsystem;
+        addRequirements(intakeSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+        intakeSubsystem.disableIntake();
+    }
+
+    @Override
+    public boolean isFinished() {
+
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+}
