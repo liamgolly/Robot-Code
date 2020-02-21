9ff475feca00f38f9ff2ec9993714a992ed6cc4f
liam <liam.golly@gmail.com>
2020-02-20 21:19:41 -0800

    "fixed" driving with my new 'drivetrain' class

---
+++ src/main/java/frc/robot/classes/DriveTrain.java
@@ -0,0 +1,27 @@
+package frc.robot.classes;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+public class DriveTrain extends DifferentialDrive {
+
+    public DriveTrain(SpeedController leftMotor, SpeedController rightMotor) {
+        super(leftMotor, rightMotor);
+    }
+
+    @Override
+    public void arcadeDrive(double power, double rotation) {
+        super.arcadeDrive(0,0);
+    }
+
+    @Override
+    public void curvatureDrive(double power, double rotation, boolean quickTurn) {
+        super.curvatureDrive(0,0,false);
+    }
+
+    @Override
+    public void tankDrive(double leftPower, double rightPower) {
+        super.tankDrive(0,0);
+    }
+
+}
--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.classes.DriveTrain;
 
 public class DrivetrainSubsystem extends SubsystemBase {
 
@@ -19,8 +20,8 @@ public class DrivetrainSubsystem extends SubsystemBase {
     WPI_TalonSRX leftMotor2 = new WPI_TalonSRX(leftMotor2ID);
     WPI_TalonSRX rightMotor1 = new WPI_TalonSRX(rightMotor1ID);
     WPI_TalonSRX rightMotor2 = new WPI_TalonSRX(rightMotor2ID);
-    DifferentialDrive driveTrain =
-            new DifferentialDrive(
+    DriveTrain driveTrain =
+             new DriveTrain(
                     new SpeedControllerGroup(leftMotor1, leftMotor2),
                     new SpeedControllerGroup(rightMotor1, rightMotor2));
 
@@ -38,24 +39,6 @@ public class DrivetrainSubsystem extends SubsystemBase {
         } else {
             drivemode = 1;
         }
-    }
-
-    public int DrivetrainMotors() {
-        int output = 0;
-        if (leftMotor1.get() != 0) {
-            output += 1;
-        }
-        if (leftMotor2.get() != 0) {
-            output += 1;
-        }
-        if (rightMotor1.get() != 0) {
-            output += 1;
-        }
-        if (rightMotor2.get() != 0) {
-            output += 1;
-        }
-
-        return output;
     }
 
     public static DrivetrainSubsystem getInstance() {
